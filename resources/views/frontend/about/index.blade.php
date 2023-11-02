1cc3dcd5336e308faba9045ca2f9d2f0856c441d
kikiisa <kikiisa89@gmail.com>
2023-11-02 15:03:16 +0800

    fix: about

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-body">
                     <h3>SISTEM INFORMASI KOS</h3>
                     <hr>
-                    @empty($record)
+                    @empty($data)
                         <div class="p-4 bg-danger text-light text-center rounded-4 fw-bold">Informasi Website Masih Kosong</div>
                     @else
                         {!! $data->deskripsi_apps !!}
